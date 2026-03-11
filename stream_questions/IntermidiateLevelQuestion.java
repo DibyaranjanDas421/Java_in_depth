e832ca06bfa608eb16a71439347e35d3fbf6cacb
DibyaranjanDas421 <dibyaranjandas468@gmail.com>
2026-03-11 21:03:50 +0530

    solved 10  stream intermidiate problem

--- stream_questions/IntermidiateLevelQuestion.java
+++ stream_questions/IntermidiateLevelQuestion.java
@@ -4,69 +4,169 @@ import java.lang.StackWalker.Option;
 import java.util.Arrays;
 import java.util.Comparator;
 import java.util.List;
+import java.util.Map;
 import java.util.Optional;
 import java.util.stream.Collector;
 import java.util.stream.Collectors;
 import java.util.stream.IntStream;
+import java.util.stream.Stream;
 
 public class IntermidiateLevelQuestion {
-    public static void main(String[] args) {
-        // 21. Find the Sum of Digits of a Number
+        public static void main(String[] args) {
+                // 21. Find the Sum of Digits of a Number
 
-        int number = 12345;
-        int sum = String.valueOf(number)
-                .chars().map(Character::getNumericValue)
-                .sum();
-        System.out.println("sum :" + sum);
+                int number = 12345;
+                int sum = String.valueOf(number)
+                                .chars().map(Character::getNumericValue)
+                                .sum();
+                System.out.println("sum :" + sum);
 
-        // 22. Find the Factorial of a Number
-        int num = 10;
-        int[] nums = new int[num];
-        for (int i = 0; i < num; i++) { // by my logic
-            nums[i] = i + 1;
+                // 22. Find the Factorial of a Number
+                int num = 10;
+                int[] nums = new int[num];
+                for (int i = 0; i < num; i++) { // by my logic
+                        nums[i] = i + 1;
+                }
+                Integer fac = Arrays.stream(nums).boxed().reduce(1, (a, b) -> a * b);
+                System.out.println("Factorial :" + fac);
+
+                int factorial = IntStream.rangeClosed(1, number)
+                                .reduce(1, (a, b) -> a * b);
+                System.out.println("Factorial: " + factorial);
+
+                // 23. Find the Second-Largest Element in a List
+                List<Integer> numbers = List.of(1, 2, 3, 4, 5);
+
+                Integer secondMax = numbers.stream()
+                                .sorted(Comparator.reverseOrder())
+                                .skip(1)
+                                .findFirst()
+                                .orElse(0);
+                System.out.println(secondMax);
+
+                // 24. Find the Second-Smallest Element in a List
+                Integer secondSmall = numbers.stream()
+                                .sorted(Comparator.naturalOrder())
+                                .skip(1)
+                                .findFirst()
+                                .orElse(0);
+                System.out.println(secondSmall);
+
+                // 25. Find the Longest String in a List
+                List<String> words = List.of("apple", "banana", "kiwi");
+
+                Optional<String> maxLengthWord = words.stream()
+                                .sorted((o1, o2) -> o2.length() - o1.length())
+                                .findFirst();
+                System.out.println(maxLengthWord.get());
+
+                String longest = words.stream()
+                                .max(Comparator.comparingInt(String::length))
+                                .orElse("");
+                System.out.println("Longest Word: " + longest);
+
+                // 26. Find the Shortest String in a List
+                String shortest = words.stream()
+                                .max(Comparator.comparingInt(String::length).reversed())
+                                .orElse("");
+                System.out.println("Longest Word: " + shortest);
+
+                // 27. Group a List of Strings by Their Length
+                Map<Integer, List<String>> res = words.stream().collect(Collectors.groupingBy(String::length));
+                System.out.println("res :" + res);
+
+                // 28. Group a List of Objects by a Specific Attribute
+                List<Person> people = List.of(
+                                new Person("Alice", 25),
+                                new Person("Bob", 30),
+                                new Person("Charlie", 25));
+
+                Map<Integer, List<Person>> gropByAge = people.stream()
+                                .collect(Collectors.groupingBy(Person::getAge));
+                System.out.println("gropByAge :" + gropByAge);
+
+                // 29. Partition a List of Integers into Even and Odd Numbers
+                Map<Boolean, List<Integer>> evenOdd = numbers.stream()
+                                .collect(Collectors.partitioningBy(x -> x % 2 == 0));
+                System.out.println("Partition a List of Integers into Even and Odd Numbers :" + evenOdd);
+
+                // 30. Merge Two Lists into a Single List
+                List<Integer> list1 = List.of(1, 2, 3);
+                List<Integer> list2 = List.of(4, 5, 6, 3);
+
+                List<Integer> merged = Stream.concat(list1.stream(), list2.stream())
+                                .collect(Collectors.toList());
+                System.out.println("Merged list :" + merged);
+
+                // 31. Find the Intersection of Two Lists
+                List<Integer> interSection = list1.stream()
+                                .filter(list2::contains)
+                                .collect(Collectors.toList());
+                System.out.println("InterSection :" + interSection);
+
+                // 32. Find the Union of Two Lists
+                List<Integer> union = Stream.concat(list1.stream(), list2.stream())
+                                .distinct()
+                                .collect(Collectors.toList());
+                System.out.println("union :" + union);
+
+                // 33. Find the Difference Between Two Lists
+                List<Integer> diffarence = Stream.concat(list1.stream(), list2.stream())
+                                .filter(n -> !list1.contains(n))
+                                .distinct()
+                                .collect(Collectors.toList());
+                System.out.println("diffarence :" + diffarence);
+
+                // 34. Count the Occurrences of Each Element in a List
+                List<String> word = List.of("apple", "banana", "apple", "orange");
+                Map<Object, Long> Occurrences = word.stream()
+                                .collect(Collectors.groupingBy(x -> x, Collectors.counting()));
+                System.out.println("Occurrences :" + Occurrences);
+
+                // 35. Count the Occurrences of Each Character in a String
+                String input = "hello";
+                Map<Object, Long> wordOccurrences = input.chars()
+                                .mapToObj(c -> (char) c)
+                                .collect(Collectors.groupingBy(x -> x, Collectors.counting()));
+
+                System.out.println("wordOccurrences :" + wordOccurrences);
+
         }
-        Integer fac = Arrays.stream(nums).boxed().reduce(1, (a, b) -> a * b);
-        System.out.println("Factorial :" + fac);
+}
 
-        int factorial = IntStream.rangeClosed(1, number)
-                .reduce(1, (a, b) -> a * b);
-        System.out.println("Factorial: " + factorial);
+class Person {
 
-        // 23. Find the Second-Largest Element in a List
-        List<Integer> numbers = List.of(1, 2, 3, 4, 5);
+        private String name;
+        private int age;
 
-        Integer secondMax = numbers.stream()
-                .sorted(Comparator.reverseOrder())
-                .skip(1)
-                .findFirst()
-                .orElse(0);
-        System.out.println(secondMax);
+        // Constructor
+        public Person(String name, int age) {
+                this.name = name;
+                this.age = age;
+        }
 
-        // 24. Find the Second-Smallest Element in a List
-        Integer secondSmall = numbers.stream()
-                .sorted(Comparator.naturalOrder())
-                .skip(1)
-                .findFirst()
-                .orElse(0);
-        System.out.println(secondSmall);
+        // Getter for name
+        public String getName() {
+                return name;
+        }
 
-        // 25. Find the Longest String in a List
-        List<String> words = List.of("apple", "banana", "kiwi");
+        // Setter for name
+        public void setName(String name) {
+                this.name = name;
+        }
 
-        Optional<String> maxLengthWord = words.stream()
-                .sorted((o1, o2) -> o2.length() - o1.length())
-                .findFirst();
-        System.out.println(maxLengthWord.get());
+        // Getter for age
+        public int getAge() {
+                return age;
+        }
 
-        String longest = words.stream()
-                .max(Comparator.comparingInt(String::length))
-                .orElse("");
-        System.out.println("Longest Word: " + longest);
+        // Setter for age
+        public void setAge(int age) {
+                this.age = age;
+        }
 
-        // 26. Find the Shortest String in a List
-        String shortest = words.stream()
-                .max(Comparator.comparingInt(String::length).reversed())
-                .orElse("");
-        System.out.println("Longest Word: " + shortest);
-    }
+        @Override
+        public String toString() {
+                return "Person{name='" + name + "', age=" + age + "}";
+        }
 }
